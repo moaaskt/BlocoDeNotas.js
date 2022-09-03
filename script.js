a7ad09a1d5e360148e4cb8e188436f990a0a4bc9
Moacir Neto <moacirmayer@hotmail.com>
2022-09-03 19:08:12 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+
+//recueprar as notas existentes no nosso navegador
+
+
+function getNotes(){
+
+
+
+}
+
+
+
+//essa função vai salvar as notas no navegador
+
+function saveNotes(notes){
+
+
+}
+
+
+
+//contruir um novo elemento para representar uma nota
+function createNoteElement(id, content){
+
+
+}
+
+
+//adicionará novas notas e salvará no localstorage
+function addNote(){
+
+
+
+}
+
+//atualizar as notas ou editar
+
+function updateNote(id, newContent){
+
+
+
+}
+
+
+
+//função deletar as notas
+
+
+function deleteNote(id, element){
+
+
+}
